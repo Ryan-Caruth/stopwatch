90a848a7f5e48e3b961b9d7c9c88423b5a254c7c
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-10-10 12:26:11 -0600

    Formatted rendered time

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,13 +1,14 @@
 import React, { useRef, useState } from "react";
+import Lap from "../lap/Lap";
 import Reset from "../reset/Reset";
 import Start from "../start/Start";
 import Stop from "../stop/Stop";
 
 const Timer = () => {
-  const [isRunning, setIsRunning] = useState(false);
-  // console.log('Is running is: ', isRunning)
-  const [isPaused, setIsPaused] = useState(false);
   const [timer, setTimer] = useState(0);
+  console.log('timer is', timer)
+  const [lap, setLap] = useState([]);
+  console.log('Lap is:', lap)
   const countRef = useRef(null);
 
   return (
@@ -15,15 +16,21 @@ const Timer = () => {
       <h1>Stopwatch</h1>
       <br />
       <div className="timer-container">
-        <h1>{timer}</h1>
-        <span id="hours">00:</span>
-        <span id="minutes">00:</span>
-        <span id="seconds">00:</span>
-        <span id="millieseconds">00</span>
+        <span id="hours">{("0" + Math.floor((timer / 360000) % 60)).slice(-2)}:</span>
+        <span id="minutes">{("0" + Math.floor((timer / 60000) % 60)).slice(-2)}:</span>
+        <span id="seconds">{("0" + Math.floor((timer / 1000) % 60)).slice(-2)}:</span>
+        <span id="millieseconds">{("0" + ((timer / 10) % 100)).slice(-2)}</span>
       </div>
-      <Start countRef={countRef} setIsRunning={setIsRunning} timer={timer} setTimer={setTimer} />
-      <Stop setIsRunning={setIsRunning} setIsPaused={setIsPaused} countRef={countRef} />
+      <Start
+        countRef={countRef}
+        timer={timer}
+        setTimer={setTimer}
+      />
+      <Stop
+        countRef={countRef}
+      />
       <Reset timer={timer} setTimer={setTimer} />
+      <Lap lap={lap} setLap={setLap} setTimer={setTimer} timer={timer} />
     </div>
   );
 };
