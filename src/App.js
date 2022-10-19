a6cc94ec467e583e906e2d3fb33ed2c0c910ac52
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-10-19 09:51:24 -0600

    Rendering clock to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import DisplayClock from "./pages/DisplayClock";
 import Stopwatch from "./pages/Stopwatch";
 
 
 function App() {
   return (
     <div>
+      <DisplayClock />
       <Stopwatch />
     </div>
   );
