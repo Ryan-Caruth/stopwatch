817655fb0365441cad9913eeb3221165fd66779e
Ryan-Caruth <ryan.caruth2@gmail.com>
2022-10-20 06:43:57 -0600

    Rendering updated time

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -3,18 +3,23 @@ import React, { useEffect, useState } from 'react'
 const Clock = () => {
     //Add a State that holds the date
     const [date, setDate] = useState(new Date());
-    //Get the current date
-        const getDate = () => {
-            setDate(new Date());
-    }
 
     //UseEffect to continually update time
-
+    useEffect(() => {
+        //Run the side effect when the component mounts
+        const updateTime = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        //Clear side effect when component unmounts
+        return () => {
+            clearInterval(updateTime)
+        }
+    }, []);
 
   return (
       <div>
-          The date is {date.toDateString()}<br />
-          The time is {date.toLocaleTimeString()}.
+          {date.toDateString()}<br />
+          {date.toLocaleTimeString()}
       </div>
   )
 }
